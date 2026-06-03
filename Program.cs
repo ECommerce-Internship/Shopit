e05a6c10e3ff0e057c1cc7496c5f648443cc8326
mariazoughaib <maria_zoughaib@live.com>
2026-06-03 15:05:17 +0300

    Add global exception handling middleware SCRUM-28

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using ECommerce.API.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();

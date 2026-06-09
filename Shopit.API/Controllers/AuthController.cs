9624654a24d53b79d84ab197d056aaa439a4cf16
mariazoughaib <maria_zoughaib@live.com>
2026-06-09 18:21:44 +0300

    fix: add LoginRequestValidator to prevent 500 on null password SCRUM-31

--- Shopit.API/Controllers/AuthController.cs
+++ Shopit.API/Controllers/AuthController.cs
@@ -3,8 +3,6 @@ using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using Shopit.Application.DTOs.Auth;
 using Shopit.Application.Interfaces;
-using Shopit.Domain.Entities;
-using Shopit.Domain.Enums;
 
 namespace Shopit.API.Controllers;
 
@@ -18,13 +16,16 @@ public class AuthController : ControllerBase
 {
     private readonly IAuthService _authService;
     private readonly IValidator<RegisterRequest> _registerValidator;
+    private readonly IValidator<LoginRequest> _loginValidator;
 
     public AuthController(
         IAuthService authService,
-        IValidator<RegisterRequest> registerValidator)
+        IValidator<RegisterRequest> registerValidator,
+        IValidator<LoginRequest> loginValidator)
     {
         _authService = authService;
         _registerValidator = registerValidator;
+        _loginValidator = loginValidator;
     }
 
     /// <summary>
@@ -49,9 +50,14 @@ public class AuthController : ControllerBase
     /// </summary>
     [HttpPost("login")]
     [ProducesResponseType(typeof(AuthResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public async Task<IActionResult> Login([FromBody] LoginRequest request)
     {
+        var validation = await _loginValidator.ValidateAsync(request);
+        if (!validation.IsValid)
+            return BadRequest(validation.Errors.Select(e => e.ErrorMessage));
+
         var response = await _authService.LoginAsync(request);
         return Ok(response);
     }
---
+++ Shopit.Application/Validators/LoginRequestValidator.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+using Shopit.Application.DTOs.Auth;
+
+namespace Shopit.Application.Validators;
+
+public class LoginRequestValidator : AbstractValidator<LoginRequest>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress().WithMessage("Email must be a valid format.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters.");
+    }
+}

49fc202fbcdb82b8c89a898b2860484a8b43a52c
suzannewadih <suzannewadihh@gmail.com>
2026-06-09 23:49:11 +0300

    Serilog config is after builder, read SEQ URL from configuration

--- Shopit.API/Program.cs
+++ Shopit.API/Program.cs
@@ -1,104 +1,89 @@
-using Microsoft.OpenApi;
 using Serilog;
-using Serilog.Enrichers;
 using Serilog.Events;
 using Shopit.API.Middleware;
 using Microsoft.EntityFrameworkCore;
 using Shopit.Infrastructure.Data;
 using StackExchange.Redis;
+using Microsoft.OpenApi;
 
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Information()
-    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-    .Enrich.FromLogContext()
-    .Enrich.WithMachineName()
-    .WriteTo.Console()
-    .WriteTo.Seq("http://localhost:5341")
-    .CreateLogger();
+var builder = WebApplication.CreateBuilder(args);
 
-try
+// Serilog configured AFTER builder so it can read from configuration
+builder.Host.UseSerilog((context, config) =>
 {
-    Log.Information("Starting Shopit API");
+    config
+        .MinimumLevel.Information()
+        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+        .Enrich.FromLogContext()
+        .Enrich.WithMachineName()
+        .WriteTo.Console()
+        .WriteTo.Seq(context.Configuration["Seq:ServerUrl"]!);
+});
 
-    var builder = WebApplication.CreateBuilder(args);
+const string DevelopmentCorsPolicy = "DevelopmentCorsPolicy";
 
-    builder.Host.UseSerilog();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
-    const string DevelopmentCorsPolicy = "DevelopmentCorsPolicy";
-
-    builder.Services.AddControllers();
-    builder.Services.AddEndpointsApiExplorer();
-
-    builder.Services.AddSwaggerGen(options =>
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
     {
-        options.SwaggerDoc("v1", new OpenApiInfo
-        {
-            Title = "Shopit API",
-            Version = "v1"
-        });
+        Title = "Shopit API",
+        Version = "v1"
+    });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(DevelopmentCorsPolicy, policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://localhost:3000",
+                "https://localhost:3000",
+                "http://localhost:4200",
+                "https://localhost:4200",
+                "http://localhost:5173",
+                "https://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+    ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("Redis")!));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Shopit API v1");
     });
 
-    builder.Services.AddCors(options =>
-    {
-        options.AddPolicy(DevelopmentCorsPolicy, policy =>
-        {
-            policy
-                .WithOrigins(
-                    "http://localhost:3000",
-                    "https://localhost:3000",
-                    "http://localhost:4200",
-                    "https://localhost:4200",
-                    "http://localhost:5173",
-                    "https://localhost:5173")
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
-    });
+    app.UseCors(DevelopmentCorsPolicy);
 
-    builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-    builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
-        ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("Redis")!));
-
-    var app = builder.Build();
-
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI(options =>
-        {
-            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Shopit API v1");
-        });
-
-        app.UseCors(DevelopmentCorsPolicy);
-
-        using var scope = app.Services.CreateScope();
-        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-        DbInitializer.Seed(context);
-    }
-
-    // Logs every HTTP request automatically
-    app.UseSerilogRequestLogging(options =>
-    {
-        options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
-        {
-            diagnosticContext.Set("ClientIp", httpContext.Connection.RemoteIpAddress?.ToString());
-            diagnosticContext.Set("UserId", httpContext.User?.FindFirst("sub")?.Value ?? "anonymous");
-        };
-    });
-
-    app.UseMiddleware<ExceptionHandlingMiddleware>();
-    app.UseHttpsRedirection();
-    app.MapControllers();
-    app.Run();
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    DbInitializer.Seed(context);
 }
-catch (Exception ex)
+
+app.UseSerilogRequestLogging(options =>
 {
-    Log.Fatal(ex, "Application terminated unexpectedly");
-}
-finally
-{
-    Log.CloseAndFlush();
-}
+    options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+    {
+        diagnosticContext.Set("ClientIp", httpContext.Connection.RemoteIpAddress?.ToString());
+        diagnosticContext.Set("UserId", httpContext.User?.FindFirst("sub")?.Value ?? "anonymous");
+    };
+});
 
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();

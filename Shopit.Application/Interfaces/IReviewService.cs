32db757e2821082b8a566776f4fed3e6b9c3e88a
suzannewadih <suzannewadihh@gmail.com>
2026-07-02 12:00:37 +0300

    Add GetAllReviewsAsync to ReviewService and IReviewService; add admin GET /reviews endpoint

--- Shopit.Application/Interfaces/IReviewService.cs
+++ Shopit.Application/Interfaces/IReviewService.cs
@@ -7,6 +7,7 @@ public interface IReviewService
     Task<ProductReviewsResponse> GetByProductIdAsync(int productId, ReviewQueryParameters parameters);
     Task<ReviewResponse> SubmitReviewAsync(SubmitReviewRequest request, int currentUserId);
     Task<ReviewResponse> UpdateReviewAsync(int reviewId, UpdateReviewRequest request, int currentUserId);
+    Task<ProductReviewsResponse> GetAllReviewsAsync(ReviewQueryParameters parameters);
     Task DeleteReviewAsync(int reviewId, int currentUserId);
     Task AdminDeleteReviewAsync(int reviewId);
 }
